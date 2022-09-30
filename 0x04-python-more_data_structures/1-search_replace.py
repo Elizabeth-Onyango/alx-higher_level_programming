2617fb3bce0337fa611c8a616ad12d42858e7a2d
Elizabeth-Onyango <lizz.onyango@gmail.com>
2022-09-30 08:27:21 -0700

    Correctionfor Task 1 and 3

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    return set_1 & set_2
+def search_replace(my_list, search, replace):
+    def s_r_elm(elm):
+        return (elm if elm != search else replace)
+    return list(map(s_r_elm, my_list))
